497e3379c32669c776e25be410f52a2d6e0bae57
hvuser <hvuser@utca-control1.phys.ufl.edu>
2021-05-26 16:32:10 -0400

    adding impact scripts

---
+++ ise/generate_mcs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#export XILINX=F:\\Xilinx\\14.7\\ISE_DS\\ISE
+#export XILINX=C:\\Xilinx\\10.1\\ISE
+export IMPACT=/home/hvuser/software/xilinx/14.7/ISE_DS/ISE/bin/lin64/impact
+source /home/hvuser/software/xilinx/14.7/ISE_DS/settings64.sh
+
+#dnames="alct_LX150T_576mirror_dav"
+dnames="alct_LX150_192 alct_LX100_288  alct_LX100_288bn  alct_LX100_288bp  alct_LX100_288fp  alct_LX100_384  alct_LX100_384mirror  alct_LX150T_576mirror  alct_LX150T_672"
+for d in $dnames
+do
+	cd $d
+	impact -batch $d.mcs.impact
+	cd ..
+done
+
